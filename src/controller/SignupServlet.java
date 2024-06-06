4347f928dc47489fb6b6836cc3b0970bb049696b
EXCITE-1901-03\soshi nakamura <soshi.nakamura@excite-software.co.jp>
2024-06-06 09:50:07 +0900

    初回のコミット
    新規登録のためのサーブレット
    ロールにuserを付与する

---
+++ src/controller/SignupServlet.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Dao.SignupDao;
+import exception.loginException;
+import model.User;
+
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+
+        String userId = request.getParameter("userId");
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+
+        //ロールをuserに設定する
+        if(role == null || role.isEmpty()) {
+        	role = "user";
+        }
+
+        String nextPage = null;
+
+        // 新規ユーザーの作成
+        User newUser = new User(userId, userName, password, role);
+
+        try {
+            // ユーザー登録処理
+            SignupDao signupDao = new SignupDao();
+            signupDao.registerUser(newUser);
+
+            // 登録成功時はセッションにユーザーオブジェクトを格納し、メインメニュー画面へ
+            HttpSession session = request.getSession();
+            session.setAttribute("user", newUser);
+
+            nextPage="mainMenu.jsp";
+
+
+        } catch (loginException | SQLException e) {
+            // 登録失敗時はエラーメッセージをリクエストに格納し、新規登録画面にフォワード
+        	String message = e.getMessage();
+			request.setAttribute("message", message);
+			request.setAttribute("error", "true");
+
+            nextPage ="signup.jsp";
+        }
+            RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+            dispatcher.forward(request, response);
+
+    }
+}
+
