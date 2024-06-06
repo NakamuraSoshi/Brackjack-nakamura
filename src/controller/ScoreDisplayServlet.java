91b16638b40ab7d45d5e890d02626952f7cb71ab
EXCITE-1901-03\soshi nakamura <soshi.nakamura@excite-software.co.jp>
2024-06-06 09:48:36 +0900

    初回のコミット
    ユーザーのスコアと上位５名のスコアを表示するためのサーブレット
    ユーザーと上位５名のスコア情報を取得し、scoreDispay.jspに転送する

---
+++ src/controller/ScoreDisplayServlet.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Dao.ScoreDao;
+import exception.loginException;
+import model.User;
+import model.UserScore;
+
+//ユーザーのスコアと上位５名のスコアを表示するためのサーブレット
+@WebServlet("/ScoreDisplayServlet")
+public class ScoreDisplayServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+    	
+    	//セッションの取得とユーザーオブジェクトの取得
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        //ログインしていないユーザを排除
+        if (user == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
+
+        try {
+        	
+        	//インスタンス化
+            ScoreDao scoreDao = new ScoreDao();
+            
+            //getUserScoreメソッドを呼び出し、ログインしているスコアを取得
+            UserScore userScore = scoreDao.getUserScore(user.getUserId());
+            
+            //getTopUsersメソッドで上位５名のスコアを取得
+            List<UserScore> topUsers = scoreDao.getTopUsers();
+
+            //取得した情報をリクエスト属性に設定し、転送
+            request.setAttribute("userScore", userScore);
+            request.setAttribute("topUsers", topUsers);
+
+            request.getRequestDispatcher("scoreDisplay.jsp").forward(request, response);
+            
+        } catch (loginException | SQLException e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+    }
+}
+
