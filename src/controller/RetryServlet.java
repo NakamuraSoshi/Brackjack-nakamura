9d4c783dc43bf211a9af2c56a37ec4aef9a946af
EXCITE-1901-03\soshi nakamura <soshi.nakamura@excite-software.co.jp>
2024-06-07 09:08:25 +0900

    clean 不要なコメントを削除しました

--- src/controller/RetryServlet.java
+++ src/controller/RetryServlet.java
@@ -13,12 +13,7 @@ public class RetryServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 現在のセッションを無効にして、新しいセッションを開始
-//        HttpSession session = request.getSession();
-//        session.invalidate();
-//
-//        // 新しいセッションを開始して、BlackjackServletにリダイレクト
-//        session = request.getSession(true);
+
         response.sendRedirect("BlackjackServlet?action=newgame");
     }
 
