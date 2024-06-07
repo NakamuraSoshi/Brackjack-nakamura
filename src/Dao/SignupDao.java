ca9b1afcb425aa590ac99a770362e0ffbe291c5e
EXCITE-1901-03\soshi nakamura <soshi.nakamura@excite-software.co.jp>
2024-06-07 17:47:16 +0900

    change Chipクラスをインポートし、chipsテーブルにデータを挿入
    新たにChipクラスをインポートして、chipsテーブルに初期値としてチップを１００に設定するようにしました。

--- src/Dao/SignupDao.java
+++ src/Dao/SignupDao.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import exception.loginException;
+import model.Chip;
 import model.User;
 
 //データベースに新しいユーザーを登録する
@@ -18,8 +19,8 @@ public class SignupDao extends BaseDao {
         PreparedStatement ps = null;
 
         try {
-            // SQL文の準備
-            String sql = "INSERT INTO user (user_id, user_name, user_password, role, chips) VALUES (?, ?, ?, ?, ?)";
+            // ユーザー情報をuserテーブルに挿入
+            String sql = "INSERT INTO user (user_id, user_name, user_password, role) VALUES (?, ?, ?, ?)";
             ps = con.prepareStatement(sql);
 
             // パラメータの設定
@@ -27,13 +28,17 @@ public class SignupDao extends BaseDao {
             ps.setString(2, user.getUserName());
             ps.setString(3, user.getUserPassword());
             ps.setString(4, user.getRole());
-            ps.setInt(5, user.getChips());
 
             // SQL文の実行
             int rowsInserted = ps.executeUpdate();
 
             if (rowsInserted > 0) {
                 System.out.println("新しいユーザーが追加されました。");
+
+             // ユーザーのチップ情報を chips テーブルに挿入
+                Chip chip = new Chip(user.getUserId(), 100);
+                addChips(chip);
+
             } else {
                 System.out.println("ユーザーの追加に失敗しました。");
             }
@@ -41,6 +46,24 @@ public class SignupDao extends BaseDao {
        }
 
     }
-}
 
+
+//チップを chips テーブルに追加するメソッド
+    public void addChips(Chip chip) throws SQLException {
+    	PreparedStatement ps = null;
+
+    	try {
+    		String sql = "INSERT INTO chips (user_id, chip_count) VALUES (?, ?)";
+    		ps = con.prepareStatement(sql);
+    		ps.setString(1, chip.getUserId());
+    		ps.setInt(2, chip.getChipCount());
+
+    		ps.executeUpdate();
+    	} finally {
+    		if (ps != null) {
+    			ps.close();
+        }
+    }
+}
+}
 
