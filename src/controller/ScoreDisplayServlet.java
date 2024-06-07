6a308fe0df743120c7aa030c2fd187e40786cbe9
EXCITE-1901-03\soshi nakamura <soshi.nakamura@excite-software.co.jp>
2024-06-07 17:50:20 +0900

    add チップの導入
    Chipクラスをインポートして、チップの情報をjspに転送するようにしました

--- src/controller/ScoreDisplayServlet.java
+++ src/controller/ScoreDisplayServlet.java
@@ -11,8 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import Dao.ChipDao;
 import Dao.ScoreDao;
 import exception.loginException;
+import model.Chip;
 import model.User;
 import model.UserScore;
 
@@ -23,7 +25,7 @@ public class ScoreDisplayServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     		throws ServletException, IOException {
-    	
+
     	//セッションの取得とユーザーオブジェクトの取得
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
@@ -35,22 +37,26 @@ public class ScoreDisplayServlet extends HttpServlet {
         }
 
         try {
-        	
+
         	//インスタンス化
             ScoreDao scoreDao = new ScoreDao();
-            
+
             //getUserScoreメソッドを呼び出し、ログインしているスコアを取得
             UserScore userScore = scoreDao.getUserScore(user.getUserId());
-            
+
+            ChipDao chipDao = new ChipDao();
+            Chip chip = chipDao.getChipsByUserId(user.getUserId());
+
             //getTopUsersメソッドで上位５名のスコアを取得
             List<UserScore> topUsers = scoreDao.getTopUsers();
 
             //取得した情報をリクエスト属性に設定し、転送
             request.setAttribute("userScore", userScore);
+            request.setAttribute("chip", chip);
             request.setAttribute("topUsers", topUsers);
 
             request.getRequestDispatcher("scoreDisplay.jsp").forward(request, response);
-            
+
         } catch (loginException | SQLException e) {
             e.printStackTrace();
             throw new ServletException(e);
