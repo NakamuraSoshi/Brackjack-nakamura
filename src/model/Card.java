b7d788bcdeb0559dc4a7161c1e51f30d5965d44c
EXCITE-1901-03\soshi nakamura <soshi.nakamura@excite-software.co.jp>
2024-06-10 17:52:48 +0900

    add 画像パスを取得するメソッドの追加
    ゲーム画面でトランプの写真を使いたいので、追加しました

--- src/model/Card.java
+++ src/model/Card.java
@@ -14,9 +14,23 @@ public class Card {
 		return rank.getValue();
 	}
 
+
 	//toStringメソッドで、カードのランクとスートの組み合わせた文字列を返す
 	@Override
 	public String toString() {
 		return rank + "of" + suit;
 	}
+
+	public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+ // 画像パスを取得するメソッド
+    public String getImagePath() {
+        return "Cards/" + rank.name().toLowerCase() + "_of_" + suit.name().toLowerCase() + ".png";
+    }
 }
